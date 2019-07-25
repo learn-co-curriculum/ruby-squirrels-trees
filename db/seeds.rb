8995f04842a50bdc2dde992300be11c816bd54ac
Tyler Knapp <tgknapp11@gmail.com>
2019-07-25 14:41:12 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Tree.create(height_in_ft: 360, tree_type: "redwood")
 Tree.create(height_in_ft: 75, tree_type: "mahogany")
 
 Hideout.create(squirrel_id: 1, tree_id: 1)
-Hideout.create(squirrel_id: 2, tree_id: 1)
+# Hideout.create(squirrel_id: 2, tree_id: 1)
 Hideout.create(squirrel_id: 3, tree_id: 1)
 Hideout.create(squirrel_id: 1, tree_id: 2)
 Hideout.create(squirrel_id: 2, tree_id: 2)
